f6e2a950ebff9778b3214003ec301ecd095918d0
Васильев Дмитрий <lotus33497@yandex.ru>
2020-10-12 11:31:31 +0700

    1.1. Входные данные в тесте [#386278]

---
+++ test/ru/job4j/calculate/X2Test.java
@@ -0,0 +1,51 @@
+package ru.job4j.calculate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class X2Test {
+
+    @Test
+    public void whenA1B1C1X1Then() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int out = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenA0B1C1X1Then() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 2;
+        int out = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenA1B1C0X1Then() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 2;
+        int out = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenA1B1C1X0Then() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 1;
+        int out = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, out);
+    }
+}
