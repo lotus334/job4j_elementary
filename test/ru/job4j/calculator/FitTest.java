4f856a57da09c3d840133c18a9362217a4803047
Васильев Дмитрий <lotus33497@yandex.ru>
2020-10-12 12:32:13 +0700

    3. Тесты для определения расстояния между точками. [#386275]

--- test/ru/job4j/calculator/FitTest.java
+++ test/ru/job4j/calculator/FitTest.java
@@ -35,5 +35,4 @@ public class FitTest {
         double out = Fit.womanWeight(in);
         Assert.assertEquals(expected, out, 0.01);
     }
-
 }
---
+++ test/ru/job4j/condition/PointTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+    @Test
+    public void when00To20Then2() {
+        int expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when00To34Then5() {
+        double expected = 5;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 3;
+        int y2 = 4;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when43To21Then2828() {
+        double expected = 2.828;
+        int x1 = 4;
+        int y1 = 3;
+        int x2 = 2;
+        int y2 = 1;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when00To00Then0() {
+        double expected = 0;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when0Min3ToMin40Then0() {
+        double expected = 5;
+        int x1 = 0;
+        int y1 = -3;
+        int x2 = -4;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
