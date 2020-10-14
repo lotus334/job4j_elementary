bf91a67ec6fcf92eaaf5b97fe7493888fdef40b7
Васильев Дмитрий <lotus33497@yandex.ru>
2020-10-14 08:13:30 +0700

    5.6. Простые числа [#386298]

---
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+
+    public static int calc(int finish) {
+        int count = 0;
+        if (finish >= 2) {
+            for (int number = 2; number <= finish; number++) {
+                if (CheckPrimeNumber.check(number)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
