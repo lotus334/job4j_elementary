600bb0636d5b94642eda543b0031cfffc92126d3
Васильев Дмитрий <lotus33497@yandex.ru>
2020-10-14 10:55:59 +0700

    6.0.2. Заполнение массива. [#386221]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -10,8 +10,8 @@ public class ArrayDefinition {
         names[1] = "Arkadiy";
         names[2] = "Suyzana";
         names[3] = "Akakiy";
-        for (int i = 0; i < names.length; i++) {
-            System.out.println(names[i]);
+        for (String name : names) {
+            System.out.println(name);
         }
         System.out.println(ages.length);
         System.out.println(surnames.length);
