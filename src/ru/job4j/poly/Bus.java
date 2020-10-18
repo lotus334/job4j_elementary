0bc106b96e5839f13c2e61b91abb1b0f825b4f46
Васильев Дмитрий <lotus33497@yandex.ru>
2020-10-18 08:53:29 +0700

    3. Интерфейс Input [#386351]

---
+++ src/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void drive() {
+        return;
+    }
+
+    @Override
+    public void passengers(int numberOfPassengers) {
+        return;
+    }
+
+    @Override
+    public int fillUp(int fuel) {
+        return 0;
+    }
+}
