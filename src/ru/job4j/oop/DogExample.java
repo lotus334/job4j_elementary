3f57a6355bbd387e9cc2818d14fd948ca34c19cb
Васильев Дмитрий <lotus33497@yandex.ru>
2020-10-23 07:47:10 +0700

    4. final [#386389]

---
+++ src/ru/job4j/oop/DogExample.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class DogExample {
+    private PetExample pet = new PetExample();
+
+    public void sound() {
+        this.pet.sound();
+    }
+}
---
+++ src/ru/job4j/oop/PetExample.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class
+PetExample {
+    private String name = "I'm pet, not a dog or cat";
+
+    public void sound() {
+        System.out.println(this.name);
+    }
+}
